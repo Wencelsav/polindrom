90f251186e11eeeb30f38a702f36b2ea8d3f949c
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2017-09-29 23:04:43 +0300

    Update 001.cpp

--- 001.cpp
+++ 001.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    string com;
+    int res=1;
+    cin>>com;
+    for(int i=0; i <com.length()/2;i++){
+        if (com[i]!=com[com.length()-i-1]){
+            res=0;
+            break;
+        }};
+    cout<<res;
+    return 0;
+}
