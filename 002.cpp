9ac3996d18ed3a3d68ce5af46e6c8953d0b1741d
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2017-10-01 13:55:03 +0300

    Update 002.cpp

--- 002.cpp
+++ 002.cpp
@@ -1,1 +1,29 @@
-
+#include "stdafx.h"
+#include <iostream>
+#include <string.h>
+#include <conio.h>
+ 
+int main(int argc, char *argv[])
+{ 
+char oldstr[128];
+char newstr[128];
+gets(oldstr);
+int cout=0;
+while(oldstr[cout++]!='\0')
+printf("%c", *strlwr(&oldstr[cout]));
+return 0;
+}
+#include <iostream>
+using namespace std;
+int main() {
+    string com;
+    int res=1;
+    cin>>com;
+    for(int i=0; i <com.length()/2;i++){
+        if (com[i]!=com[com.length()-i-1]){
+            res=0;
+            break;
+        }};
+    cout<<res;
+    return 0;
+}
